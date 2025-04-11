95290707e928dedcd409bba5ebdb71958711ad0b
NandoJunioDev <Frnndmaz_cf@outlook.com>
2025-04-11 17:27:30 -0300

    first commit

---
+++ src/main/java/com/passeio/PasseioDeLancha/PasseioDeLanchaApplication.java
@@ -0,0 +1,13 @@
+package com.passeio.PasseioDeLancha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasseioDeLanchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasseioDeLanchaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passeio/PasseioDeLancha/PasseioDeLanchaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passeio.PasseioDeLancha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasseioDeLanchaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
